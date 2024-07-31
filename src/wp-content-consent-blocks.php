4eaacefff68978e582c43f8a79b104f64144e7ee
Magnus Leßmann (@MarkL4YG) <github@m-lessmann.de>
2024-07-31 09:58:10 +0200

    :tada: Initialize new wordpress plugin project

---
+++ src/wp-content-consent-blocks.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WP Content Consent Blocks
+ * Description: A wordpress plugin which adds blocks to dynamically show content after the viewer consented to it.
+ * Version: 1.0.0
+ * Plugin URI: https://github.com/MarkL4YG/wp-content-consent-blocks#readme
+ * Requires at least: 6.0
+ * Requires PHP: 8.0
+ * Author: MarkL4YG
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+ */
+
